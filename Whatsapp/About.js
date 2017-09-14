7ca1ed27b82e6b2d4488e92f40c46c9e5a0f93bc
SwiftEsther <swiftestherduro@gmail.com>
2017-09-14 19:04:10 +0100

    Clone

--- Whatsapp/About.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('whatsApp',[]);
-app.controller('myCtrl',function($scope){
-	$scope.ourMission='WhatsApp started as an alternative to SMS. Our product now supports sending and receiving a variety of media: text, photos, videos, documents, and location, as well as voice calls. Our messages and calls are secured with end-to-end encryption, meaning that no third party including WhatsApp can read or listen to them. Behind every product decision is our desire to let people communicate anywhere in the world without barriers.";
-});
--- Whatsapp/Home.js
+++
@@ -1,5 +0,0 @@
-var app = angular.modeule('whatsApp',[]);
-app.controller("myCtrl", function($scope){
-	
-
-});
