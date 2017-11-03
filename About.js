db52fd7c9b7c06925d5316c01c44afa7ef2df97c
SwiftEsther <swiftestherduro@gmail.com>
2017-11-03 09:26:04 +0000

    Add files via upload

---
+++ About.js
@@ -0,0 +1,6 @@
+var app = angular.module('whatsApp',[]);
+app.controller('aboutCtrl',function($scope){
+$scope.ourApp="More than 1 billion people in over 180 countries use WhatsApp1 to stay in touch with friends and family, anytime and anywhere. WhatsApp is free2 and offers simple, secure, reliable messaging and calling, available on phones all over the world.<br>And yes, the name WhatsApp is a pun on the phrase What's Up.";
+$scope.ourMission="WhatsApp started as an alternative to SMS. Our product now supports sending and receiving a variety of media: text, photos, videos, documents, and location, as well as voice calls. Our messages and calls are secured with end-to-end encryption, meaning that no third party including WhatsApp can read or listen to them. Behind every product decision is our desire to let people communicate anywhere in the world without barriers.";
+$scope.ourTeam="WhatsApp was founded by Jan Koum and Brian Acton who had previously spent 20 years combined at Yahoo. WhatsApp joined Facebook in 2014, but continues to operate as a separate app with a laser focus on building a messaging service that works fast and reliably anywhere in the world.";
+});
