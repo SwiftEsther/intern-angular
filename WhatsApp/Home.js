2d1209de6e9eaed4a2a7167495c89bea9814962e
SwiftEsther <swiftestherduro@gmail.com>
2017-11-14 15:46:13 +0000

    Please review

---
+++ WhatsApp/Home.js
@@ -0,0 +1,5 @@
+var app = angular.module('whatsApp',[]);
+app.controller("homeCtrl", function($scope){
+	$scope.text="With voice calls, you can talk to your friends and family for free*, even if they're in another country. And with free* video calls, you can have face-to-face conversations for when voice or text just isn't enough. WhatsApp voice and video calls use your phone's Internet connection, instead of your cell plan's voice minutes, so you don't have to worry about expensive calling charges.";
+
+});
